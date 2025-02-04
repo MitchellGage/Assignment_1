e6a916cc2b31b5490329605d48f92edd80127e03
MitchellGage <mitchellrgage@gmail.com>
2025-02-04 12:57:18 -0600

    Missing Timer Decorator

--- fib.py
+++ fib.py
@@ -1,1 +1,21 @@
+from functools import lru_cache
 
+#fib.py
+@lru_cache
+#@timer
+def fib(n: int) -> int:
+  if n == 0 or n == 1:
+    return n
+  lastNum = 0
+  currNum = 1
+  nextNum = 1
+  for i in range(n-1):
+    currNum = nextNum
+    nextNum = currNum + lastNum
+    lastNum = currNum
+  print(nextNum)
+  return nextNum
+  
+
+if __name__ == "__main__":
+  fib(100)
