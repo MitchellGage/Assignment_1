77da55a5da3f7442d1f57d873d412cc00b0236a9
MitchellGage <mitchellrgage@gmail.com>
2025-02-04 12:24:34 -0600

    Finished Echo Program

--- echo.py
+++ echo.py
@@ -1,1 +1,18 @@
+#echo.py
 
+
+def echo(text: str, repetitions: int = 3) -> str:
+  """Imitate a real world echo"""
+  if len(text) >= repetitions:
+    for i in range(repetitions):
+      i = -1*(i-repetitions)
+      echo = ""
+      for j in range(i):
+        echo = echo + text[len(text) - i + j]
+      print(echo)
+  return "."
+
+
+if __name__ == "__main__":
+  text = input ("Yell something at a mountain: ")
+  print(echo(text))
