8f5d361864aa648061444ace8dc3fa2dfbce2e17
MitchellGage <mitchellrgage@gmail.com>
2025-02-04 22:41:49 -0600

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,21 +1,30 @@
+import matplotlib.pyplot as plot
 from functools import lru_cache
+import time
 
-#fib.py
+# fib.py
 @lru_cache
-#@timer
 def fib(n: int) -> int:
-  if n == 0 or n == 1:
-    return n
-  lastNum = 0
-  currNum = 1
-  nextNum = 1
-  for i in range(n-1):
-    currNum = nextNum
-    nextNum = currNum + lastNum
-    lastNum = currNum
-  print(nextNum)
-  return nextNum
-  
+    startTime = time.time()
+    if n == 0 or n == 1:
+        return n
+    print("Finished in ", time.time() - startTime, "s: f(0) -> 0")
+    print("Finished in ", time.time() - startTime, "s: f(1) -> 1")
+    lastNum = 0
+    currNum = 0
+    nextNum = 1
+    y = [0, 0]
+    for i in range(n - 1):
+        currNum = nextNum
+        nextNum = currNum + lastNum
+        lastNum = currNum
+        dur = time.time() - startTime
+        y.append(dur)
+        print(f"Finished in {dur} s: f({i + 2}) -> {nextNum}")
+    x = list(range(0, 101))
+    plot.plot(x, y)
+    plot.show()
+    return nextNum
 
 if __name__ == "__main__":
-  fib(100)
+    fib(100)
